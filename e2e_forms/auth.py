905b587f9836fa77af41eadbaf5fdbfd3e387411
Jonas Schulz <jonas@jschulz.de>
2023-02-23 22:24:38 +0100

    Reworked password hashing

--- e2e_forms/auth.py
+++ e2e_forms/auth.py
@@ -1,21 +1,36 @@
 import os
-from flask import Blueprint, redirect, render_template, request, url_for
+from typing import Tuple
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from scrypt import hash as scrypt_hash
+import scrypt
+from base64 import b64encode, b64decode
 
-from e2e_forms.models.user import User
+from e2e_forms.models.user import User, UserStatus
 from . import db
+from . import app
 
 auth = Blueprint('auth', __name__)
 
-def hash_password(password):
-    #generate random salt
-    salt = os.urandom(32)
-    return (salt, scrypt_hash(password, salt, maxtime=0.1))
+def hash_password(password: str) -> bytes:
+    """Hash a password for storing.
+    :param password: The password to hash.
+    :return: A chunk of random bytes encrypted with the password."""
+    salt = os.urandom(app.config['SCRYPT_SALT_LENGTH'])
+    return scrypt.encrypt(salt, password.encode('utf-8'), app.config['SCRYPT_MAX_TIME'])
 
-def verify_password(password, salt, hashed_password):
-    return scrypt_hash(password, salt) == hashed_password
+
+def verify_password(password: str, hashed_password: bytes) -> bool:
+    """Verify a password against a stored hash.
+    :param password: The password to verify.
+    :param hashed_password: The stored password hash to compare against.
+    :return: True if the password matches the stored hash, False otherwise."""
+    try:
+        scrypt.decrypt(hash_password, password.encode('utf-8'), app.config['SCRYPT_MAX_TIME'])
+        return True
+    except scrypt.error:
+        return False
+    return False
 
 @auth.route('/login')
 def login():
@@ -29,20 +44,26 @@ def signup():
 def signup_post():
     # code to validate and add user to database goes here
     email = request.form.get('email')
-    name = request.form.get('name')
     password = request.form.get('password')
+    password = request.form.get('password_repeat')
 
-    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+    user = User.query.filter_by(email=email).first()
 
-    if user: # if a user is found, we want to redirect back to signup page so user can try again
-        return redirect(url_for('auth.signup'))
+
+    if user:
+        if user.status == UserStatus.ACTIVE:
+            flash('Email address already exists')
+            return render_template('auth/signup.html')
+        db.sesion.delete(user)          
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=hash_password(password))
+    hashed_password = hash_password(password)
+    new_user = User(email=email, password=b64encode(hashed_password).decode('utf-8'))
 
     # add the new user to the database
     db.session.add(new_user)
     db.session.commit()
+    return render_template("notice.html", notice="To complete your registration, please check your email for a confirmation link.")
 
 @auth.route('/logout')
 def logout():
