67bcfbbbcdfae05c2ce6943638503e683a2ef72b
Jonas Schulz <jonas@jschulz.de>
2023-02-23 22:25:12 +0100

    Changed model to store state of registration

--- e2e_forms/models/user.py
+++ e2e_forms/models/user.py
@@ -1,7 +1,14 @@
 from .. import db
+import enum
+
+class UserStatus(enum.Enum):
+    ACTIVE = 1
+    INACTIVE = 2
+    DELETED = 3
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+    email = db.Column(db.String(200), unique=True)
+    password = db.Column(db.Text())
+    status = db.Column(db.Enum(UserStatus), default=UserStatus.INACTIVE)
+    register_time = db.Column(db.DateTime(), default=db.func.now())
