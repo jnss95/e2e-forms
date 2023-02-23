44473739eebd22f64d1d0e4f7121eb5a32d86ff7
Jonas Schulz <jonas@jschulz.de>
2023-02-23 17:16:41 +0100

    Bootstrapped sqlalchemy

--- e2e_forms/__init__.py
+++ e2e_forms/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask, request,g 
+from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape
 import sqlite3
 app = Flask(__name__)
-from .db import query_db
+db = SQLAlchemy()
+print(app.config['ENV'])
+if (app.config['ENV'] == 'dev'):
+    app.config['SECRET_KEY'] = 'dev'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///var/lib/e2e-forms/e2e-forms.db'
+db.init_app(app)
 
+# blueprint for auth routes in our app
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
 
-
-
-from .db import get_db
 @app.route('/')
 def hello():
     for user in query_db('select * from users'):
