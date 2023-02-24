6dccd9f75c17ac758f81483c16a74ca8bb28150e
Jonas Schulz <jonas@jschulz.de>
2023-02-24 16:37:28 +0100

    Added theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,31 @@
 module.exports = {
   content: ["./e2e_forms/templates/**/*.html"],
   theme: {
+    colors: {
+      "bg-dark0": "#282828",
+      "bg-dark2": "#504945",
+      "bg-dark3": "#665c54",
+      "bg-dark4": "#7c6f64",
+
+      "fg-dark0": "#fbf1c7",
+      "fg-dark1": "#ebdbb2",
+      "fg-dark2": "#d5c4a1",
+      "fg-dark3": "#bdae93",
+      "fg-dark4": "#a89984",
+      "fg-yellow": "#d79921",
+
+      "bg-light0": "#fbf1c7",
+      "bg-light1": "#ebdbb2",
+      "bg-light2": "#d5c4a1",
+      "bg-light3": "#bdae93",
+      "bg-light4": "#a89984",
+
+      "fg-light0": "#282828",
+      "fg-light1": "#3c3836",
+      "fg-light2": "#504945",
+      "fg-light3": "#665c54",
+      "fg-light4": "#7c6f64",
+    },
     extend: {},
   },
   plugins: [],
