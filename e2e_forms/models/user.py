64f32d5a0391f129d4493403e6ac6b27c935ea8c
Jonas Schulz <jonas@jschulz.de>
2023-02-24 11:28:36 +0100

    Reformatted code

--- e2e_forms/models/user.py
+++ e2e_forms/models/user.py
@@ -1,13 +1,15 @@
 from .. import db
 import enum
 
+
 class UserStatus(enum.Enum):
     ACTIVE = 1
     INACTIVE = 2
     DELETED = 3
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(200), unique=True)
     password = db.Column(db.Text())
     status = db.Column(db.Enum(UserStatus), default=UserStatus.INACTIVE)
