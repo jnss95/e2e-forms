8f440846f7a4afbcbe18eb906ea53eb6aa72d77b
Jonas Schulz <jonas@jschulz.de>
2023-02-24 14:15:50 +0100

    Moved import because of cyclic dependency

--- e2e_forms/__init__.py
+++ e2e_forms/__init__.py
@@ -1,16 +1,19 @@
-from .auth import auth as auth_blueprint
+
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_babel import Babel
 
+# autopep8: off
 app = Flask(__name__)
 db = SQLAlchemy()
+from e2e_forms.auth import auth as auth_blueprint
 babel = Babel(app)
 print(app.config['ENV'])
 app.config.from_pyfile('config.py')
 if (app.config['DEBUG']):
     app.config['SECRET_KEY'] = 'dev'
-
+# autopep8: on
 
 db.init_app(app)
 
