03a79fdf497a0880ff6571a115a2b9b39e425ec6
Jonas Schulz <jonas@jschulz.de>
2023-02-24 11:29:08 +0100

    Removed unused imports

--- e2e_forms/__init__.py
+++ e2e_forms/__init__.py
@@ -1,12 +1,12 @@
-from flask import Flask, request,g 
+from .auth import auth as auth_blueprint
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from markupsafe import escape
-import sqlite3
+
 app = Flask(__name__)
 db = SQLAlchemy()
 print(app.config['ENV'])
 app.config.from_pyfile('config.py')
-if (app.config['ENV'] == 'dev'):
+if (app.config['DEBUG']):
     app.config['SECRET_KEY'] = 'dev'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////var/lib/e2e-forms/e2e-forms.db'
 
@@ -14,10 +14,9 @@ if (app.config['ENV'] == 'dev'):
 db.init_app(app)
 
 # blueprint for auth routes in our app
-from .auth import auth as auth_blueprint
 app.register_blueprint(auth_blueprint)
+
 
 @app.route('/')
 def hello():
-    return f'Hello!'
-
+    return 'Hello!'
